88fb4c128617fad11835d6afe8165175a205e0a0
sirisun26 <siri.sunkavalli@reisystems.com>
2024-08-06 20:00:06 -0700

    First Commit

---
+++ app/src/index.const.js
@@ -0,0 +1,6 @@
+export const routePaths = {
+    root: "/",
+    questions: "/questions",
+    report: "/report"
+}
+
---
+++ app/src/pages/Questions/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import QuestionsComp from '../../components/Layout/Questions/QuestionsLayout'
+function Questions() {
+  return (
+    <QuestionsComp/>
+  )
+}
+
+export default Questions
