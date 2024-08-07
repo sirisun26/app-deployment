69a84888993bd2e919f2aee225f46934afa1318c
sirisun26 <SIRI.SUNKAVALLI@REISYSTEMS.COM>
2024-08-06 20:12:20 -0700

    final updates

---
+++ src/router.js
@@ -0,0 +1,26 @@
+// import App from "./App"; //TODO
+// import Questions from "./pages/Questions";
+// import ReportPage from "./pages/Report";
+
+
+// import {
+//     createBrowserRouter
+//   } from "react-router-dom";
+
+// import { routePaths } from "./index.const";
+
+
+// const router = createBrowserRouter([
+//     {
+//       path: routePaths.root,
+//       element: <App />,
+//     }, {
+//         path: routePaths.questionss,
+//         element: <Questionss />
+//     }, {
+//         path: routePaths.report,
+//         element: <ReportPage />
+//     }
+//   ]);
+
+//   export default router;
